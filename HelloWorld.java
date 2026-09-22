7db75f7d6d48cb754bce22a7c0f494d27df415bc
yazh <yazhcity@gmail.com>
2026-09-22 22:43:16 +0530

    New Change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Jenkins Trigger Worked!");
+        System.out.println("Hello EWorld");
     }
 }
